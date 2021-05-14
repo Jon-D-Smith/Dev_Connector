95278bef7e76ce166a3a03066968836df1e0ecb2
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-13 22:34:45 -0500

    Added express router for api endpoints

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+
 app.listen(PORT, ()=> {
     console.log(`Connected on port ${PORT}`)
 })
