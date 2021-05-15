5a3665dfc3ce22ffae70dd79b2167837f3464b69
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-14 21:57:02 -0500

    Added validation for user post route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const connectDB = require('./config/db');
 //connect DB
 connectDB();
 
+//Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
